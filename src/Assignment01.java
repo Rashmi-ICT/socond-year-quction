5a653b7844590e5fec7f173451bf191b1b75fa42
RashmiICT <96565@fhss.sjp.ac.lk>
2021-06-02 00:00:10 +0530

    edit code

--- src/Assignment01.java
+++ src/Assignment01.java
@@ -43,8 +43,8 @@ public class Assignment01 {
                 }
             }
             //searching the runner up
-            for(Map.Entry<String, Integer> en : m.entrySet()){
-                if (en != min1 && (min2 == null || (en.getValue().compareTo(min2.getValue()) <0))) {
+            for(Map.Entry<String, Integer> en : m.entrySet()) {
+                if (en != min1 && (min2 == null || (en.getValue().compareTo(min2.getValue()) < 0))) {
                     min2 = en;
                 }
             }
