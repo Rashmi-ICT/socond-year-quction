70b0a13f7ac8500aa346193fa9fa7d51798f187e
RashmiICT <96565@fhss.sjp.ac.lk>
2021-06-02 08:37:25 +0530

    edit code

--- src/Assignment01.java
+++ src/Assignment01.java
@@ -40,12 +40,17 @@ public class Assignment01 {
             for(Map.Entry<String, Integer> entry : m.entrySet()){
                 if (min1 == null || entry.getValue().compareTo(min1.getValue()) < 0){
                     min1 = entry;
+
+                    System.out.println(entry);
+                    System.out.println(min1);
                 }
             }
             //searching the runner up
-            for(Map.Entry<String, Integer>entry : m.entrySet()) {
+            for(Map.Entry<String, Integer> entry : m.entrySet()) {
                 if (entry!= min1 && (min2 == null || (entry.getValue().compareTo(min2.getValue()) < 0))) {
                     min2 =entry;
+
+                    //System.out.println(entry);
                 }
             }
             System.out.println("\n\nWINNER OF THE MARATHON");
